6a1208c6be3fb6b8ae6313de451535b35a099cd4
hafiz9ouman <hafiz9oman.dev@gmail.com>
2024-03-18 23:52:53 +0500

    change password fixed

--- resources/views/admin/users/update_password.blade.php
+++ resources/views/admin/users/update_password.blade.php
@@ -93,7 +93,7 @@ if (count($errors) > 0){
                     </div>
                     @if(Auth::check())
                         <div class="tile-footer text-right " >
-                            <a href="{{url('users')}}" class="btn btn-primary">@lang('general.cancel')</a>
+                            <a href="{{url('site_admin')}}" class="btn btn-primary">@lang('general.cancel')</a>
                             <button type="submit" class="btn btn-success">@lang('general.save')</button>
                         </div>
                     @endif
